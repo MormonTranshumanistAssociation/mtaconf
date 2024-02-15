40c3c45c8e3b129fbcec7542720238ada6c9e3a5
Carl Youngblood <carl@youngbloods.org>
2024-02-14 23:44:03 -0700

    Update speaker list

--- src/2024/teaser.tsx
+++ src/2024/teaser.tsx
@@ -2,6 +2,14 @@ export function Teaser() {
   return (
     <div className="py-8 xl:py-16 px-4 sm:px-6 lg:px-8 bg-black overflow-hidden">
       <div className="max-w-max lg:max-w-8xl mx-auto text-white">
+        <div className="relative z-10 mb-4 md:mb-2 md:px-6">
+          <div className="text-base max-w-prose lg:max-w-none">
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-yellow-100 bg-black sm:text-4xl">
+              Speakers
+            </p>
+          </div>
+        </div>
+
         <div className="relative z-10 mb-4 md:mb-2 md:px-6 pt-8">
           <div className="flex flex-row flex-wrap gap-6">
             <div className="flex flex-col gap-4">
@@ -15,6 +23,42 @@ export function Teaser() {
                 </div>
                 <div className="text-white self-center">
                   Mormon Keynote Speaker
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/brent-allsop.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Brent Allsop
+                </div>
+                <div className="text-white self-center">Founder</div>
+                <div className="text-white self-center">Canonizer</div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/bryant-smith.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Bryant Smith
+                </div>
+                <div className="text-white self-center">Secretary</div>
+                <div className="text-white self-center">MTA</div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/caleb-jones.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Caleb Jones
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  Software Architect
+                  <br />
+                  Disney
                 </div>
               </div>
             </div>
@@ -81,6 +125,58 @@ export function Teaser() {
               </div>
             </div>
 
+            {/*<div className="flex flex-col gap-4">*/}
+            {/*  <img src="/img/2024/dan-moore.jpg" className="w-48 h-48" />*/}
+            {/*  <div className="flex flex-col">*/}
+            {/*    <div className="text-white self-center text-xl font-bold">*/}
+            {/*      Dan Moore*/}
+            {/*    </div>*/}
+            {/*    <div className="text-white self-center w-40 text-center"></div>*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/dorothee-cannon.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Dorothée Cannon
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  Interpreter
+                  <br />
+                  MTA
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2022/joseph.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Joseph West
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  Co-Founder
+                  <br />
+                  MTA
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/kouassi-nguessan.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Kouassi Nguessan
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  Area Leader
+                  <br />
+                  MTA Africa
+                </div>
+              </div>
+            </div>
+
             <div className="flex flex-col gap-4">
               <img src="/img/2024/lincoln-cannon.jpg" className="w-48 h-48" />
               <div className="flex flex-col">
@@ -107,6 +203,42 @@ export function Teaser() {
               </div>
             </div>
 
+            {/*<div className="flex flex-col gap-4">*/}
+            {/*  <img src="/img/2024/mckay-moore.jpg" className="w-48 h-48" />*/}
+            {/*  <div className="flex flex-col">*/}
+            {/*    <div className="text-white self-center text-xl font-bold">*/}
+            {/*      McKay Moore*/}
+            {/*    </div>*/}
+            {/*    <div className="text-white self-center w-40 text-center"></div>*/}
+            {/*  </div>*/}
+            {/*</div>*/}
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/micah-redding.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Micah Redding
+                </div>
+                <div className="text-white self-center w-40 text-center text-xs">
+                  Executive Director
+                  <br />
+                  Christian Transhumanist Association
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/nancy-fulda.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Nancy Fulda
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  AI Researcher
+                </div>
+              </div>
+            </div>
+
             <div className="flex flex-col gap-4">
               <img src="/img/2024/randall-paul.jpg" className="w-48 h-48" />
               <div className="flex flex-col">
@@ -115,6 +247,23 @@ export function Teaser() {
                 </div>
                 <div className="text-white self-center w-40 text-center">
                   Foundation for Religious Diplomacy
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img
+                src="/img/2024/rachael-givens-johnson.jpg"
+                className="w-48 h-48"
+              />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Rachael Johnson
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  Postdoctoral Fellow
+                  <br />
+                  Maxwell Institute
                 </div>
               </div>
             </div>
@@ -132,6 +281,20 @@ export function Teaser() {
                   Director
                   <br />
                   MTA
+                </div>
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <img src="/img/2024/tamara-kneese.jpg" className="w-48 h-48" />
+              <div className="flex flex-col">
+                <div className="text-white self-center text-xl font-bold">
+                  Tamara Kneese
+                </div>
+                <div className="text-white self-center w-40 text-center">
+                  Project Director
+                  <br />
+                  AIMLab
                 </div>
               </div>
             </div>
@@ -160,16 +323,6 @@ export function Teaser() {
                   Founding Editor
                   <br />
                   Palladium Magazine
-                </div>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-4">
-              <div className="flex flex-col">
-                <div className="h-48 flex">
-                  <div className="m-auto text-white self-center text-xl font-bold">
-                    ...and more
-                  </div>
                 </div>
               </div>
             </div>
