bda175048518618d2c751fd5722998ee580754dd
Carl Youngblood <carl@youngbloods.org>
2024-04-18 09:18:03 -0600

    Update schedule

--- src/2024/schedule.tsx
+++ src/2024/schedule.tsx
@@ -245,7 +245,7 @@ export function Schedule() {
                         1:40 pm
                       </td>
                       <td className="border-b border-stone-600 p-2 text-stone-300">
-                        God Hates Singletons
+                        Make Yourself Human Again
                       </td>
                       <td className="whitespace-nowrap border-b border-stone-600 p-2 pr-8 text-stone-300">
                         Wolf Tivy
